1220b1ea6ee885567548456a8748e0056a876ded
Yuri Pinheiro <yurii1408@hotmail.com>
2018-06-22 21:46:09 -0400

    ...

---
+++ Report/Report/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Report
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_gerar = new System.Windows.Forms.Button();
+            this.tbx_nome = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_gerar
+            // 
+            this.btn_gerar.Location = new System.Drawing.Point(208, 148);
+            this.btn_gerar.Name = "btn_gerar";
+            this.btn_gerar.Size = new System.Drawing.Size(75, 23);
+            this.btn_gerar.TabIndex = 0;
+            this.btn_gerar.Text = "Gerar";
+            this.btn_gerar.UseVisualStyleBackColor = true;
+            this.btn_gerar.Click += new System.EventHandler(this.btn_gerar_Click);
+            // 
+            // tbx_nome
+            // 
+            this.tbx_nome.Location = new System.Drawing.Point(85, 72);
+            this.tbx_nome.Name = "tbx_nome";
+            this.tbx_nome.Size = new System.Drawing.Size(321, 20);
+            this.tbx_nome.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 208);
+            this.Controls.Add(this.tbx_nome);
+            this.Controls.Add(this.btn_gerar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_gerar;
+        private System.Windows.Forms.TextBox tbx_nome;
+    }
+}
+
---
+++ Report/Report/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Microsoft.Reporting.WebForms;
+using System.IO;
+using System.Diagnostics;
+
+namespace Report
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_gerar_Click(object sender, EventArgs e)
+        {
+
+                ReportViewer reportviewer = new ReportViewer();
+                reportviewer.ProcessingMode = ProcessingMode.Local;
+
+                // Caminho para encontrar o relatorio
+                reportviewer.LocalReport.ReportEmbeddedResource = "Report.Report1.rdlc";
+
+                //Parametros do relatorio
+                List<ReportParameter> listReportParameters = new List<ReportParameter>();
+                listReportParameters.Add(new ReportParameter("nome", tbx_nome.Text));
+
+                //Passando parametros para o report
+                reportviewer.LocalReport.SetParameters(listReportParameters);
+
+                //Criando o PDF
+
+                //Renderizando o arquivo
+                Warning[] warnings;
+                string[] streamids;
+                string mimetype;
+                string encoding;
+                string extension;
+
+                byte[] bytePDF = reportviewer.LocalReport.Render("PDF", null, out mimetype, out encoding, out extension, out streamids, out warnings);
+
+                //Criando o nome do arquivo
+                FileStream filestream = null;
+                string nomearquivos = Path.GetTempPath() + "relatorio" + DateTime.Now.ToString("dd_MM_yyyy-HH_mm_ss") + ".pdf";
+
+                //Salvando o arquivo no disco
+                filestream = new FileStream(nomearquivos, FileMode.Create);
+                filestream.Write(bytePDF, 0, bytePDF.Length);
+                filestream.Close();
+
+                //Iniciando o arquivos
+                Process.Start(nomearquivos);
+
+            
+        }
+    }
+}
